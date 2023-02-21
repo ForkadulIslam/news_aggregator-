5da75311acb8078e82bb8ae83ea13d782bee7801
ForkadulIslam <forkadulislam@gmail.com>
2023-02-21 10:42:37 +0600

    Add exported SQL file

--- news_portal.sql
+++ news_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2023 at 06:56 AM
+-- Generation Time: Feb 21, 2023 at 05:37 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
